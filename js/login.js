c745adc18d5bf94da234ec31ca5431bb697d37fa
Turtle84375 <turtle84375@programmer.net>
2021-09-27 09:37:24 -1000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,25 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "TurtleCoding84" && password == "1234" ){/*Will make more secure later*/
+document.cookie = "authorized=true";
+window.location = "https://turtlecoding84.github.io/TurtleGov-Data-Center/home";
+return false;
+}
+else{
+  if ( username == "" || password == ""){
+    alert("You must type in a username and password to log in!");
+  }
+  else{
+  alert("Incorrect username or password, please retry.");
+  return false;
+}
+}
+}
 
+/*
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha512.js"></script>
+<script>
+    var hash = CryptoJS.SHA512("Message");
+</script>
+*/
