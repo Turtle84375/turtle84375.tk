780c81ca78a7eaa158ca78714e3da4baf0659348
Turtle84375 <turtle84375@programmer.net>
2021-10-28 08:57:49 -1000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,21 @@
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username.toLowerCase() == "username" && password == "1234" ){/*Will make more secure later*/
+if ( username.toLowerCase() == "turtle84375" && password == "notmyrealpass" ) { /*Will make more secure later*/
 document.cookie = "session=authorized?true, admin?true, banned?false";
 document.cookie = "id=" + username;
 window.location = "https://turtle84375.tk/dashboard";
 return false;
-}
-else{
-  if ( username == "" || password == ""){
-  document.getElementById("errorText").innerHTML = "You must type in a username and password to log in!";
-  }
-  else{
-  document.getElementById("errorText").innerHTML = "Incorrect username or password, please retry.";
+} else if ( username.toLowerCase() == "username" && password == "1234" ) {
+  document.cookie = "session=authorized?true, admin?false, banned?false";
+  document.cookie = "id=" + username;
+  window.location = "https://turtle84375.tk/dashboard";
   return false;
-}
-}
+} else if ( username == "" || password == "") {
+    document.getElementById("errorText").innerHTML = "You must type in a username and password to log in!";
+    } else {
+        document.getElementById("errorText").innerHTML = "Incorrect username or password, please retry.";
+        return false;
+    }
+  }
 }
