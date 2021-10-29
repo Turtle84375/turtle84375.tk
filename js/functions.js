86a611d2a979d7fda599c199f58ac0836f27e856
Turtle84375 <turtle84375@programmer.net>
2021-10-29 11:35:29 -1000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,10 @@
 function loginLogoutText() {
+  var loginDynamicURL;
   var x = document.cookie;
   if (x.includes("authorized?true")) {
+    loginDynamicURL = "https://turtle84375.tk/dashboard";
     document.getElementById("loginDynamic").innerHTML = "Dashboard";
+  } else {
+    loginDynamicURL = "https://turtle84375.tk/login";
   }
 }
